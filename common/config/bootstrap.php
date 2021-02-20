85aa7ddde8e8957e6d6fec36aefdc6adc1866f13
denis909 <denis909@mail.ru>
2021-02-20 11:51:23 +0300

    n/a

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,4 +4,4 @@ use denis909\yii\CascadeConfig;
 
 Yii::setAlias('@denis909/glide', dirname(dirname(__DIR__)));
 
-CascadeConfig::addPath(__DIR__);
+CascadeConfig::addPath(dirname(dirname(__DIR__)));
